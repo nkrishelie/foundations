872ebe4464cb0a210e3a20174c1910c150f18cea
nkrishelie <60033261+nkrishelie@users.noreply.github.com>
2025-11-30 20:49:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // Вместо жестких алиасов используем dedupe.
+    // Это говорит Vite: "Если разные пакеты просят 'three', дай им всем одну и ту же копию".
+    dedupe: ['three', 'react', 'react-dom'],
+  },
+  // Опционально: отключаем слишком строгие проверки чанков, если они мешают
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 })
